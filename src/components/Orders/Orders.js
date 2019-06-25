6fc5966336dfcb3e119960b034aaac74f30148cf
Jeressia Williamson <jeressiajaytv@gmail.com>
2019-06-25 18:49:17 -0500

    three columns

---
+++ src/components/Orders/Orders.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+import './Orders.scss';
+
+export default class Orders extends Component {
+  render() {
+    return (
+      <div>
+        <h3>Orders</h3>
+      </div>
+    );
+  }
+}
