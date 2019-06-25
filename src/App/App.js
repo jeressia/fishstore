648aa5c6071722c17931e308b97d0835e15816f5
Jeressia Williamson <jeressiajaytv@gmail.com>
2019-06-24 20:28:17 -0500

    unmount

--- src/App/App.js
+++ src/App/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import firebase from 'firebase/app';
 
 import Auth from '../components/Auth/Auth';
 import Home from '../components/Home/Home';
@@ -13,6 +14,20 @@ fbConnection();
 export default class App extends React.Component {
   state = {
     authed: false,
+  }
+
+  componentDidMount() {
+    this.removelistener = firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({ authed: true });
+      } else {
+        this.setState({ authed: false });
+      }
+    });
+  }
+
+  componentWillUnmount() {
+    this.removelistener();
   }
 
   render() {
