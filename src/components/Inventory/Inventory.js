9ecc6d14baa914ac2378ee34ba6236582a6602b7
Jeressia Williamson <jeressiajaytv@gmail.com>
2019-06-29 13:42:47 -0500

    delete works

--- src/components/Inventory/Inventory.js
+++ src/components/Inventory/Inventory.js
@@ -1,23 +1,14 @@
 import React, { Component } from 'react';
 
-import fishData from '../../helpers/data/fishData';
-import Fish from '../Fish/Fish';
+
+import AllFish from '../Fish/Fish';
 import './Inventory.scss';
 
 export default class Inventory extends Component {
-  state = {
-    fishes: [],
-  }
-
-  componentDidMount() {
-    fishData.getFishes()
-      .then(fishes => this.setState({ fishes }))
-      .catch(err => console.error('uh-oh, fishes', err));
-  }
 
   render() {
-    const fishComponents = this.state.fishes.map(fish => (
-      <Fish key={fish.id} fish={fish}/>
+    const fishComponents = this.props.fishes.map(fish => (
+      <AllFish key={fish.id} fish={fish}/>
     ));
 
     return (
